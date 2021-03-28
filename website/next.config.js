3ef5478489b0909a946999344e2bf72ef8fa3e98
ChristopherGarvin <51714053+ChristopherGarvin@users.noreply.github.com>
2021-03-28 15:56:32 +0100

    Update next.config.js

--- website/next.config.js
+++ website/next.config.js
@@ -3,7 +3,7 @@ const withCSS = require("@zeit/next-css")
 module.exports = withCSS({
   env: {
     "DEVELOPMENT_API_URL": "http://localhost:3000",
-    "PRODUCTION_API_URL": "api.bud-news.com",
+    "PRODUCTION_API_URL": "https://api.bud-news.com",
     "ALGOLIA_APP_ID": "",
     "ALGOLIA_PUBLIC_API_KEY": "",
     "PRODUCT_NAME": "Bud News"
